d20ea9a15cb4525ce7cb5149699a6346279fd559
RadMom <RadMom1111@gmail.com>
2023-01-16 11:03:39 +0200

    morgan added

--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,10 +1,15 @@
 const express=require("express");
+const morgan=require("morgan");
+
+//Need to set-up DB now
 
 const app=express();
 const port=3000;
 
 app.set("view engine","ejs");
 app.use(express.static("public"))
+
+app.use(morgan("tiny"))
 
 app.get("/",(req,res)=>{
     res.render("home");
