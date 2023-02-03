bf32bd55c402e53905d52142e861f61fa9dabb1b
RadMom <RadMom1111@gmail.com>
2023-02-03 08:55:04 +0200

    DB added

---
+++ EXPRESS/routes/pageRoutes.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const pageController=require("../controllers/pageController");
+
+const router=express.Router();
+
+router.get("/",);
+router.get("/:id",);
+router.get("/create",);
+router.post("/create",)
+router.delete("/:id",);
+
+//second argument will be pageController
+
+module.exports=router;
--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,19 +1,32 @@
-const express=require("express");
-const morgan=require("morgan");
+const express=require("express");           
+const morgan=require("morgan");             //https://www.npmjs.com/package/morgan
+require("dotenv").config();
 
-//Need to set-up DB now
+//MongoDB set-up
+const mongoose=require('mongoose');
+const db=process.env.MONGO_URL
+console.log(db);
+mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(result=>app.listen(3000,()=>{
+console.log('db is connected and server is live on port 3000');
+}))
+.catch(err=>{
+    console.log(`Some ERR here - ${err}`);
+});
 
 const app=express();
 const port=3000;
 
-app.set("view engine","ejs");
-app.use(express.static("public"))
+app.set("view engine","ejs");               //https://ejs.co/
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan("tiny"))
+app.use(morgan("tiny"))                     
 
 app.get("/",(req,res)=>{
     res.render("home");
 });
+
 
 app.get("/about",(req,res)=>{
     res.render("about");
@@ -25,8 +38,4 @@ app.get("/create",(req,res)=>{
 
 app.use((req,res)=>{
     res.status(404).render("404");
-})
-
-app.listen(port,()=>{
-    console.log(`Server is live on port ${port}`);
 })
